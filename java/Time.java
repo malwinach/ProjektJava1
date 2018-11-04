4586a5db8ff20ff56a9f2b974fc12745430588de
Malwina <chmalwina3@gmail.com>
2018-11-04 12:36:10 +0100

    npz

---
+++ java/Time.java
@@ -0,0 +1,13 @@
+//import java.time.DayOfWeek;
+import java.time.LocalTime;
+//import java.time.LocalDateTime;
+//import org.joda.time.Minutes;
+
+public class Time {
+    public static void main(String[] args){
+        LocalTime now1 = LocalTime.now();
+      //  Minutes periodTime = Minutes.minutesBetween(countSince, countTo);
+
+      //  System.out.print(now1);
+    }
+}
